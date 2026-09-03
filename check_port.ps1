9ce2b66678b0b50ca047def604324f765d98ae85
linzhongyang <linzhongyang@dxwsoft.net>
2026-09-03 17:16:56 +0800

    chore: 移除端口诊断临时脚本

--- check_port.ps1
+++
@@ -1,7 +0,0 @@
-Get-NetTCPConnection -LocalPort 8000 -State Listen -ErrorAction SilentlyContinue | ForEach-Object {
-  $pidToCheck = $_.OwningProcess
-  $proc = Get-Process -Id $pidToCheck -ErrorAction SilentlyContinue
-  $name = if ($proc) { $proc.ProcessName } else { "dead" }
-  $path = if ($proc) { $proc.Path } else { "unknown" }
-  Write-Output ("PID=" + $pidToCheck + " NAME=" + $name + " PATH=" + $path)
-}
